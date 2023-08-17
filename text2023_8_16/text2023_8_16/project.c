d49b1c249384516249d588e6c415f5fb9e445acd
WangDeLang <1165690537@qq.com>
2023-08-17 15:23:44 +0800

    试验

---
+++ text2023_8_16/text2023_8_16/project.c
@@ -0,0 +1,115 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+
+//int main(void)
+//{
+//	int num;
+//	num = 1;
+//
+//	printf("\tI am a simple ");
+//	printf("computer.\n");
+//	printf("\tMy favorite number is %d because it is first .\n", num);
+//	getchar();//作用：让程序等待击键，运行窗口会在下一个按键按下才关闭
+//	return 0;
+//}
+
+//int main()
+//{
+//	int feet, fathoms;
+//
+//	fathoms = 2;
+//	feet = 6 * fathoms;
+//	printf("There are %d feet in %d fathoms!\n", feet, fathoms);
+//	printf("Yes,I said %d feet!\n", 6 * fathoms);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a;
+//	int b;
+//
+//	a = 5;
+//	b = 2;
+//	b = a;
+//	a = b;
+//	printf("%d %d\n", b, a);
+//
+//	return 0;
+//}
+
+//int main()
+//{
+//	float w;//float为浮点数数据类型
+//	float v;
+//
+//	printf("a");
+//	printf("b");
+//	printf("c\n");
+//
+//	scanf("%f", &w);
+//
+//	v = 1700.0 * w * 14.5833;
+//	printf("%.2f\n", v);//%.xf表示保留x位小数
+//}
+
+//int main()
+//{
+//	char a = 65;
+//	char b = 'A';
+//	printf("%c %c %d", a,b,b);//字符（char）类型用%c打印出来%d只能打印整数
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	float a, b;
+//
+//	b = 2.0e4 + 1.0;
+//	printf("%f\n", b);
+//	a = b - 2.0e20;
+//	printf("%f\n", a);
+//}
+
+//int main()
+//{
+//	printf("%zd\n", sizeof(int));
+//	printf("%zd\n", sizeof(char));
+//	printf("%zd\n", sizeof(long));
+//	printf("%zd\n", sizeof(long long));
+//	printf("%zd\n", sizeof(double));
+//	printf("%zd\n", sizeof(long double));
+//	return 0;
+//}
+
+//int main()
+//{
+//	int n = 4;
+//	int m = 5;
+//	float f = 7.0f;
+//	float g = 8.0f;
+//
+//	printf("%d\n", n, m);//就打印n
+//	printf("%d %d %d\n", n, m);//多一个随机数
+//	printf("%d %d\n", f, g);//把整数部分打印出来//%d显示float类型的值时不会被转换成int类型8
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("abc");
+//	return 0;
+//}
+
+//int main()
+//{
+//	printf("            ");
+//	return 0;
+//}
+
